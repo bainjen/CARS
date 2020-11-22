984c9663a59e2fff034a52a7e55a70c6e344ec7c
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-11-21 19:37:31 -0700

    logic out calculatePlaneTime function, incorporating the varoious rock generating, placing, and maneuvering functions

--- helpers/simCalculations.js
+++ helpers/simCalculations.js
@@ -14,6 +14,37 @@ export const placeRandomRock = (options) => {
   return { x, y };
 };
 
+// calculates the number of square feet the auger will miss in order to go around a rock
+// and the amount of extra square feet it moves to navigate rocks
+// assuming the combine turns at a 90 degree angle
+export const calculateRockManeuver = (augerLength, rock) => {
+  const topThreshold = augerLength,
+    bottomThreshold = options.fieldLength - augerLength,
+    leftThreshold = augerLength,
+    rightThreshold = options.fieldLength - augerLength;
+
+  const feetAroundRock = 3;
+  let extraMiss = 0;
+
+  if (topThreshold >= rock.y) {
+    extraMiss += rock.y;
+  }
+  if (bottomThreshold <= rock.y) {
+    extraMiss += options.fieldLength - rock.y;
+  }
+  if (leftThreshold >= rock.x) {
+    extraMiss += 2 * augerLength * rock.x;
+  }
+  if (rightThreshold <= rock.x) {
+    extraMiss += 2 * augerLength * (options.fieldLength - rock.x);
+  }
+
+  return {
+    extraSquareFeet: feetAroundRock,
+    missedSquareFeet: extraMiss + 1,
+  };
+};
+
 export const generateRocks = (numRocks) => {
   let rocks = [];
   for (let i = 0; i < numRocks; i++) {
@@ -25,13 +56,25 @@ export const generateRocks = (numRocks) => {
 //3 extra square feet per rock
 //  Increase in Wheel Size by 1-inch results in weight increase by 5% but a time reduction of
 // 3%.
-export const calculatePlaneTime = (wheelSize, augerLength) => {
+export const calculatePlaneTime = (wheelSize, augerLength, rocksArr) => {
+  let rockMiss = 0;
+  let rockAdd = 0;
+  rocksArr.forEach((rock) => {
+    const { extraSquareFeet, missedSquareFeet } = calculateRockManeuver(
+      augerLength,
+      rock
+    );
+    rockMiss += Math.sqrt(missedSquareFeet);
+    rockAdd += Math.sqrt(extraSquareFeet);
+  });
+  const extraPassLength = rockAdd - rockMiss;
+  const extraPassProportion = extraPassLength / options.fieldLength;
   const numPasses = Math.ceil(options.fieldLength / augerLength);
   const passTime =
     options.basePassTime -
     options.basePassTime * ((wheelSize - options.baseWheels) * 0.03);
   //returns time in seconds
-  return numPasses * passTime;
+  return (numPasses + extraPassProportion) * passTime;
 };
 
 export const calculateFieldPercentage = () => {};
