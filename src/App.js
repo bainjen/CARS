25640f5916d3b61188b9b28e65cab52f7a3b5f1d
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-11-22 20:09:09 -0700

    basic styling for improved readability

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { createConfiguration } from "./graphql/mutations";
 import { listConfigurations } from "./graphql/queries";
 // import Field from "./components/Field";
-// import Auger from "./components/inputs/Auger";
+import Auger from "./components/inputs/Auger";
 // import Fuel from "./components/inputs/Fuel";
 // import Input from "./components/inputs/Input";
 // import { CombineProvider } from "./components/CombineContext";
@@ -14,8 +14,28 @@ Amplify.configure(awsExports);
 
 const initialState = { wheelSize: "", augerLength: "", fuelType: "" };
 
+const InputDiv = styled.div`
+  margin: 1em;
+  border: solid black 2px;
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`;
+
+const ConfigDiv = styled.div`
+  margin: 1em;
+  border: solid black 2px;
+  width: 50%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
 const AppDiv = styled.div`
   margin: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 function App() {
@@ -59,32 +79,40 @@ function App() {
 
   return (
     <AppDiv>
-      <h2>Combine Configurations</h2>
-      <input
-        onChange={(event) => setInput("wheelSize", event.target.value)}
-        // style={styles.input}
-        value={formState.wheelSize}
-        placeholder="wheel size"
-      />
-      <input
-        onChange={(event) => setInput("augerLength", event.target.value)}
-        // style={styles.input}
-        value={formState.augerLength}
-        placeholder="auger length"
-      />
-      <input
-        onChange={(event) => setInput("fuelType", event.target.value)}
-        // style={styles.input}
-        value={formState.fuelType}
-        placeholder="fuelType"
-      />
-      <button onClick={addConfiguration}>Create Combine Configuration</button>
+      <h2>Create New Combine Configuration</h2>
+      <InputDiv>
+        {/* <Auger /> */}
+        <input
+          onChange={(event) => setInput("wheelSize", event.target.value)}
+          // style={styles.input}
+          value={formState.wheelSize}
+          placeholder="wheel size"
+        />
+        <input
+          onChange={(event) => setInput("augerLength", event.target.value)}
+          // style={styles.input}
+          value={formState.augerLength}
+          placeholder="auger length"
+        />
+        <input
+          onChange={(event) => setInput("fuelType", event.target.value)}
+          // style={styles.input}
+          value={formState.fuelType}
+          placeholder="fuelType"
+        />
+        <button onClick={addConfiguration}>Add</button>
+      </InputDiv>
+
+      <h2>My Combines:</h2>
       {configurations.map((configuration, index) => (
-        <div key={configuration.id ? configuration.id : index}>
-          <p>{configuration.wheelSize}</p>
-          <p>{configuration.augerLength}</p>
-          <p>{configuration.fuelType}</p>
-        </div>
+        <ConfigDiv key={configuration.id ? configuration.id : index}>
+          <p>Wheel Size: {configuration.wheelSize} </p>
+          <span> | </span>
+          <p>Auger Length: {configuration.augerLength} </p>
+          <span> | </span>
+          <p>Fuel Type: {configuration.fuelType}</p>
+          {/* <button>show data</button> */}
+        </ConfigDiv>
       ))}
       {/* <CombineProvider>
         <Input />
